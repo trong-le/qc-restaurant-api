8f3ea19f41b4d5487120932dfa90866b19d816c9
Trong Le <trong.pe.le.92@gmail.com>
2018-01-10 17:17:51 -0500

    Added error handling in top-level, added geocode to request, push restaurant to db

--- app.js
+++ app.js
@@ -8,6 +8,17 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(require('./controllers'));
 
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
 app.listen(port, () => {
   console.log('Listening on port ' + port)
 });
--- controllers/addRestaurant.js
+++ controllers/addRestaurant.js
@@ -1,9 +1,19 @@
 const { Router } = require('express');
 const { Restaurant } = require('../models');
 const { getRatings } = require('../utils/yelp');
+const { geocode } = require('../utils/geocoder');
 
 const router = new Router();
 
 router.post('/', async (req, res, next) => {
-    req.body.rating = await getRatings();
+    try {
+        const { result } = geocode(req.address);
+
+        // Need to handle error in case object keys dont exist
+        req.body.rating = await getRatings(result.geometry.location.lat, result.geometry.location.lng);
+        const restaurant = Restaurant.create(req.body);
+        res.status(201).send({ restaurant, success: true});
+    } catch (err) {
+        res.status(409).send();
+    }
 });
